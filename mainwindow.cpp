753fdc27bb90429ddcf5c04fa9bd5d9ca680ce5e
Mina Melad <102828808+minamelad33333@users.noreply.github.com>
2023-04-18 22:54:45 +0200

    added main gui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,7 +3,7 @@
 #include "ui_mainwindow.h"
 #include "lib/gui_lib.h"
 #include<QString>
-
+#include <QPixmap>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,8 +13,11 @@ MainWindow::MainWindow(QWidget *parent)
     this->showMaximized();
     ;
     GUI_lib::setUpWindow(this, "Chat Vibes", ":/imgs/logo.png");
-
-
+    int width = ui->Textimg2->width() ;
+    int height= ui->Textimg2->height() ;
+    QPixmap myPixmap(":/icons/D:/Downloads/undraw_new_message_re_fp03.svg");
+    ui->Textimg2->setPixmap(myPixmap.scaled(width,height,Qt::KeepAspectRatio));
+    // Set the width of the pixmap using a style sheet
 
 }
 
